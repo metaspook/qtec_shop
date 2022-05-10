22d70e7fdc4f57c5c9d1b78a5091b9e3a3c0c79c
metaspook <metaspook@gmail.com>
2022-05-10 16:16:06 +0600

    add searchbar ui, grid improvment

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -6,19 +6,61 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GridView.builder(
-          padding: const EdgeInsets.all(8),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+            padding: const EdgeInsets.only(left: 20, right: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                const Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search_rounded,
+                      color: Colors.grey,
+                    ))
+              ],
+            ),
           ),
-          itemCount: 20,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Text('Item $index'),
-            );
-          },
-        ),
+          Expanded(
+            child: GridView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.only(
+                    left: 6,
+                    right: 6,
+                    top: 15,
+                    bottom: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text('Item $index'),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
